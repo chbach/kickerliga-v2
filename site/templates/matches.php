926a2495237adecc0f0ff79c4afae3d69b0cb837
Christoph Bach <christoph-bach@gmx.de>
2014-02-03 13:11:09 +0100

    don’t use short_open_tags

--- site/templates/matches.php
+++ site/templates/matches.php
@@ -11,48 +11,46 @@
 <section class="landing-section" role="main">
 	<div class="pure-g-r centered">
 		<div class="pure-u-1 l-box">
-			<h1 class="no-margin"><?php echo html($page->title()) ?></h1>
+			<h1 class="no-margin"><?php echo html($page->title()); ?></h1>
 		</div>
 		<div class="pure-u-2-3 l-box">
 			<article>
-			    <?php echo kirbytext($page->text()) ?>
+			    <?php echo kirbytext($page->text()); ?>
 			</article>
 
-			<? foreach ($matches as $index => $match ): ?>
+			<?php foreach ($matches as $index => $match ): ?>
 			<?php
 				$odd_or_even = ('odd' == $odd_or_even) ? 'even' : 'odd';
 			?>
 
-			<?php if ($day != $match->day):
-				$day = $match->day;
-			?>
-			<?php if ($day > 1): ?> </table></div><?php endif ?>
+			<?php if ($day != $match->day):	$day = $match->day;?>
+			<?php if ($day > 1): ?> </table></div><?php endif; ?>
 
 			<div class="spieltag">
-			<h3 class="toggle"><?php echo $day ?>. Spieltag</h3>
-			<table cellpadding="0" cellspacing="0" class="w100 table">
-				<tr>
-					<th class="w230p">Team 1</th>
-					<th class="w230p">Team 2</th>
-					<th class="w80p">Ergebnis</th>
-				</tr>
-			<? endif; ?>
+				<h3 class="toggle"><?php echo $day ?>. Spieltag</h3>
+				<table cellpadding="0" cellspacing="0" class="w100 table">
+					<tr>
+						<th class="w230p">Team 1</th>
+						<th class="w230p">Team 2</th>
+						<th class="w80p">Ergebnis</th>
+					</tr>
+			<?php endif; ?>
 
-				<?php if ($location != $match->location):
-					$location = $match->location;
-					$odd_or_even = 'even'; ?>
-				<tr class="subhead"><td colspan="5">
-					<?php echo $location ?>
-					<small><?php echo date("d.m.Y H:i", strtotime($match->date)); ?></small>
-				</td></tr>
-				<?php endif; ?>
-				<tr class="<?= $odd_or_even ?>">
-					<td><?php echo $match->team1 ?></td>
-					<td><?php echo $match->team2 ?></td>
-					<td><?php echo $match->sets ?></td>
-				</tr>
-			<? endforeach; ?>
-			</table>
+					<?php if ($location != $match->location):
+						$location = $match->location;
+						$odd_or_even = 'even'; ?>
+					<tr class="subhead"><td colspan="5">
+						<?php echo $location; ?>
+						<small><?php echo date("d.m.Y H:i", strtotime($match->date)); ?></small>
+					</td></tr>
+					<?php endif; ?>
+					<tr class="<?php echo $odd_or_even ?>">
+						<td><?php echo $match->team1; ?></td>
+						<td><?php echo $match->team2; ?></td>
+						<td><?php echo $match->sets; ?></td>
+					</tr>
+			<?php endforeach; ?>
+				</table>
 			</div>
 
 		</div>
