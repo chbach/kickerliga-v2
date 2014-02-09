7cf82d948f2d276b0952616a8dcf0f27eda289a2
Christoph Bach <christoph-bach@gmx.de>
2014-02-09 18:24:44 +0100

    adds panel

---
+++ site/panel/blueprints/home.php
@@ -0,0 +1,19 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# default blueprint
+
+title: Page
+pages: true
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  links:
+    label: Linke Spalte
+    type:  textarea
+    size:  medium
+ 	rechts:
+    label: Rechte Spalte
+    type:  textarea
+    size:  medium
---
+++ site/panel/blueprints/termine.php
@@ -0,0 +1,19 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# default blueprint
+
+title: Page
+pages: true
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type:  textarea
+    size:  large
+  events:
+    label: Events
+    type:  textarea
+    size:  large
