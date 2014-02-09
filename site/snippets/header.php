bdb8e320c8652d633ffa09020e91131604c0a8e1
Christoph Bach <christoph-bach@gmx.de>
2014-02-09 20:51:58 +0100

    adds favicon tag

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,6 +8,7 @@
 	<meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
 	<meta name="robots" content="index, follow" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="http://www.kickerliga-paderborn.de/favicon.ico"/>
 
 	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 	<?php echo css('assets/css/main.css') ?>
