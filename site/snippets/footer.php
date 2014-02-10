51c145711f969b6cf13f569992af185a10e4d551
Christoph Bach <christoph-bach@gmx.de>
2014-02-10 11:33:46 +0100

    speed optimizations

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -6,13 +6,16 @@
 	</div>
 </footer>
 <script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-40628249-1', 'kickerliga-paderborn.de');
-ga('send', 'pageview');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40628249-1', 'kickerliga-paderborn.de');
+  ga('send', 'pageview');
+
 </script>
 
 </body>
 </html>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700,300' rel='stylesheet' type='text/css'>
