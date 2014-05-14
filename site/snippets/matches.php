a585720d066878de855168a2f859c87053c01725
Christoph Bach <christoph-bach@gmx.de>
2014-05-14 11:36:32 +0200

    adds support for detailed results in pokal view

--- site/snippets/matches.php
+++ site/snippets/matches.php
@@ -13,9 +13,10 @@
 	?>
 		<h3><?php echo $location; ?></h3>
 	<?php endif; ?>
+	<?php $img = ""; if (isset($match['Img'])) { $img = $images->find($match['Img']); } ?>
 	<div class="match cf">
 		<span class="team"><?php echo $match['Team1']; ?></span>
-		<span class="result"><?php echo ($match['Ergebnis'])? $match['Ergebnis'] : '--:--' ; ?></span>
+		<span class="result"><?php if ($img): ?><a href="<?php echo $img->url() ?>" class="fresco"><?php endif; ?> <?php echo ($match['Ergebnis'])? $match['Ergebnis'] : '--:--' ; ?><?php if ($img): ?></a><?php endif; ?></span>
 		<span class="team"><?php echo $match['Team2']; ?></span>
 	</div>
 
--- site/templates/pokal.php
+++ site/templates/pokal.php
@@ -1,11 +1,11 @@
 <?php snippet('header') ?>
-
 <section class="landing-section" role="main">
 	<div class="pure-g-r centered">
 		<div class="pure-u-2-3 l-box">
 			<?php snippet('matches', array(
 			  		'title' => $page->title(),
 			  		'text' => $page->text(),
+			  		'images' => $page->images(),
 			  		'matches' => yaml($page->matches()
 			  	)));
 			 ?>
@@ -17,5 +17,4 @@
 		</div>
 	</div>
 </section>
-
 <?php snippet('footer') ?>
